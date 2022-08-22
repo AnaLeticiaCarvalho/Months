54dd958f4470d98c057d37422276743d3f47f6bd
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2022-08-22 16:07:13 -0300

    Write the month's number, the aplication will print month's name

---
+++ month.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int monthNum;
+
+    cout << "Digite o número do mês: ";
+    cin >> monthNum;
+
+    if (monthNum==1)
+    {
+        cout << "Janeiro";
+    }
+
+    else if (monthNum==2)
+    {
+        cout << "Fevereiro";
+    }
+
+    else if (monthNum==3)
+    {
+        cout << "Março";
+    }
+
+    else if (monthNum==4)
+    {
+        cout << "Abril";
+    }
+    
+    else if (monthNum==5)
+    {
+        cout << "Maio";
+    }
+
+    else if (monthNum==6)
+    {
+        cout << "Junho";
+    }
+    else if (monthNum==7)
+    {
+        cout << "Julho";
+    }
+
+    else if (monthNum==8)
+    {
+        cout << "Agosto";
+    }
+
+    else if (monthNum==9)
+    {
+        cout << "Setembro";
+    }
+    
+    else if (monthNum==10)
+    {
+        cout << "Outubro";
+    }
+
+    else if (monthNum==11)
+    {
+        cout << "Novembro";
+    }
+
+     else if (monthNum==12)
+    {
+        cout << "Dezembro";
+    }
+
+    else
+    {
+        cout << "Opção inválida!";
+    }
+
+    
+    
+    
+    
+
+
+    
+
+
+
+}
